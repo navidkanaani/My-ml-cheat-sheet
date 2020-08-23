46453d7a80e28c24cdab10921497838f2b7aa3fd
navidkanaani <navidkaen@gmail.com>
2020-08-23 13:58:14 +0430

    added until sklearn

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/kaen/Projects/Machine_learning/test_env/env/lib"
+XML2_LIBS="-lxml2 -L/home/kaen/Projects/Machine_learning/test_env/env/lib -lz -L/home/kaen/Projects/Machine_learning/test_env/env/lib -llzma -lpthread -L/home/kaen/Projects/Machine_learning/test_env/env/lib -L/home/kaen/Projects/Machine_learning/test_env/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/kaen/Projects/Machine_learning/test_env/env/include/libxml2 -I/home/kaen/Projects/Machine_learning/test_env/env/include"
+MODULE_VERSION="xml2-2.9.10"
+
